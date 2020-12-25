1a1c1712fcca8ee1b8b479a920a174b97779058a
swastik nanda <swastik.nanda@gmail.com>
2020-12-26 01:32:51 +0530

    P-3 to P-9 completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const CardArray = [];
-
+var addTaskID;
 var popUpAddList = document.querySelector(".popup-add-list");
 console.log(popUpAddList);
 var add = document.querySelector(".add-button");
@@ -40,20 +40,24 @@ function addList(){
     blur1.classList.toggle("blur-effect-add-list1");
 
     var addList = document.createElement('div');
-
-    
+    CardObj.addTaskIdentifier = "addTaskID"+(CardArray.length+1);
+    var addTaskID = CardObj.addTaskIdentifier;
 
     addList.innerHTML=`<div class="card-list">
                         <div class="heading-card-list">
                             ${listHeading}<hr style="border: 0px;background-color: blue;height: 3px;">
-                            <div class="footer-card-list">
-                                <button class="add-button-card-list">
-                                    <i class="fa fa-trash-o" style="color: blue;"></i>
-                                </button>
-                                <button class="add-button-card-list">
-                                    <i class="fa fa-plus-circle" style="color: blue;"></i>
-                                </button>
+                        </div>
+                        <div class="task-list">
+                            <div class="task-row">
                             </div>
+                        </div>
+                        <div class="footer-card-list">
+                            <button class="add-button-card-list">
+                                <i class="fa fa-trash-o" style="color: blue;"></i>
+                            </button>
+                            <button class="add-button-card-list" id=${addTaskID} onclick="addTaskPopUp(this.id)">
+                                <i class="fa fa-plus-circle" style="color: blue;"></i>
+                            </button>
                         </div>
                     </div>`;
 
@@ -62,3 +66,46 @@ function addList(){
     CardArray.push(CardObj);
 }
 
+
+
+//Add tasks
+function addTaskPopUp(taskID){
+    addTaskID = taskID;
+    var blurTask = document.querySelector('.card-list');
+    blurTask.classList.add("blur-effect-task");
+    document.querySelector(".popup-add-task").style.display = 'block';
+}
+
+function closeTaskPopUp(){
+    var blurTask = document.querySelector('.card-list');
+    blurTask.classList.remove("blur-effect-task");
+    document.querySelector(".popup-add-task").style.display = 'none';
+}
+
+function addTask(){
+    console.log(addTaskID);
+    var idTask = addTaskID.charAt(addTaskID.length-1);
+    var getArrayIndex = Number(idTask) - 1;
+    console.log(getArrayIndex);
+    var taskName = document.querySelector("#input-add-task").value;
+    if(taskName == ''){
+        alert("Please Enter the Task Name");
+        return;
+    }
+    var TaskObj = {}
+    TaskObj.taskTitle = taskName;
+    TaskObj.isDone = false;
+
+    CardArray[getArrayIndex].TaskList = TaskObj;
+    
+    var addTask = document.createElement('div');
+    addTask.className = "task-row";
+    addTask.innerHTML = `<div class="task-name">${taskName}</div>
+                        <div><button class="mark-done">Mark As Done</button></div>`
+    //document.querySelector('.task-list').appendChild(addTask);
+    document.getElementById(addTaskID).parentElement.previousElementSibling.appendChild(addTask);
+
+    var blurTask = document.querySelector('.card-list');
+    blurTask.classList.remove("blur-effect-task");
+    document.querySelector(".popup-add-task").style.display = 'none';
+}
---
+++ test.js
@@ -0,0 +1,35 @@
+var blurTask = document.querySelector('.card-list');
+let getId;
+
+function addTaskPopUp(tempVar){
+    getId = tempVar;
+    blurTask.classList.add("blur-effect-task");
+    document.querySelector(".popup-add-task").style.display = 'block';
+}
+
+function closeTaskPopUp(){
+    blurTask.classList.remove("blur-effect-task");
+    document.querySelector(".popup-add-task").style.display = 'none';
+}
+
+function addTask(){
+    console.log(getId);
+    var taskName = document.querySelector("#input-add-task").value;
+    if(taskName == ''){
+        alert("Please Enter the Task Name");
+        return;
+    }
+    
+    var addTask = document.createElement('div');
+
+    addTask.innerHTML = `<div class="task-row">
+                            <div class="task-name">${taskName}</div>
+                            <div><button class="mark-done">Mark As Done</button></div>
+                        </div>`
+    document.querySelector('.task-list').appendChild(addTask);
+
+    var blurTask = document.querySelector('.card-list');
+    blurTask.classList.remove("blur-effect-task");
+    document.querySelector(".popup-add-task").style.display = 'none';
+}
+
